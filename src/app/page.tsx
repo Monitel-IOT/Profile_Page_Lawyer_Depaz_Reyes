6b752559138a35ef662c174cbbf935c5049fe538
Cristian Benites Condori <78431820+cb3ndev@users.noreply.github.com>
2023-11-29 21:05:09 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
             width="100%"
             height="100%"
             style={{ border: 0 }}
-            allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
