6559633d745e8da9c95d06dd05732b912420836d
Cristian Benites Condori <78431820+cb3ndev@users.noreply.github.com>
2023-12-04 22:25:53 -0500

    feat: single articles view

--- src/app/articulos/[id]/page.tsx
+++ src/app/articulos/[id]/page.tsx
@@ -1,59 +1,48 @@
 "use client"
 
-import React from 'react';
-// Importa otros componentes si los necesitas, como Layout, Header, Footer, etc.
+import React, { useEffect, useState } from 'react';
 import Article from '../../../components/article/article';
-import Title from '@/components/UI/atoms/Title/Title';
+import { ArticleData } from '@/interface/ArticleData';
 
+const ArticlePage = ({ params }: { params: { id: string } }) => { // Asume que 'id' se pasa como prop
 
-const mockArticles = [
-  {
-    id: '1',
-    title: 'El primer artículo',
-    imageUrl: 'https://oficinascentenario.pe/wp-content/uploads/2021/06/DSC02889-1024x683.jpg',
-    content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content he making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsu will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
-    summary: 'ste es el resumen del primer  ste es el resumen del primer  Este es el resumen del primer artículo... Este es el resumen del primer artículo... Este es el resumen del primer artículo... Este es el resumen del primer artículo...',
-    createdAt: 'Nov 13, 2014' // Formato de fecha como string
-  },
-  {
-    id: '2',
-    title: 'El segundo artículo',
-    imageUrl: 'https://www.ofitipo.com/modules/dbblog/views/img/post/tendencias-oficinas-2023.png',
-    content: 'Este es el contenido del segundo artículo...',
-    summary: 'Este es el resumen del primer artículo...',
-    createdAt: 'Nov 13, 2014' // Formato de fecha como string
-  },
-  {
-    id: '3',
-    title: '3er artículo',
-    imageUrl: 'https://www.ofitipo.com/modules/dbblog/views/img/post/tendencias-oficinas-2023.png',
-    content: 'Este es el contenido del 3 artículo...',
-    summary: 'Este es el resumen del primer artículo...',
-    createdAt: 'Nov 13, 2014' // Formato de fecha como string
-  },
-  // ...más artículos
-];
-
-const ArticlePage = ({ params }: { params: { id: string } }) => {
-  
   const { id } = params;
+  const [article, setArticle] = useState<ArticleData | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // Asegúrate de que id es un string antes de comparar
-  const articleId = Array.isArray(id) ? id[0] : id;
+  useEffect(() => {
+    if (!id) return; // Asegúrate de que el id esté presente
 
-  // Encuentra el artículo por ID
-  const article = mockArticles.find((article) => article.id === articleId);
+    const fetchArticle = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`/api/articles/${id}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setArticle(data.data);
+      } catch (error) {
+        console.error('Error fetching article:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  // Asegúrate de manejar el caso de que el artículo no se haya encontrado
+    fetchArticle();
+  }, [id]); // Dependencia de id para que el efecto se ejecute cuando el id cambie
+
+  if (isLoading) {
+    return <p>Cargando artículo...</p>;
+  }
+
   if (!article) {
     return <p>Artículo no encontrado</p>;
   }
 
   return (
     <div className='bg-black'>
-      {/* Puedes envolver tu página con un Layout si tienes uno */}
       <Article key={article.id} {...article} />
-      {/* Footer, si tienes uno */}
     </div>
   );
 };
--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import Title from '@/components/UI/atoms/Title/Title';
 import Text from '@/components/UI/atoms/Text/Text';
+import { ArticleData } from '@/interface/ArticleData';
 
-interface ArticleProps {
-  id: string;
-  title: string;
-  imageUrl: string;
-  summary: string;
-  content: string;
-  createdAt: string;
-}
-
-const Article = ({ title, imageUrl, content, createdAt }: ArticleProps) => {
+const Article = ({ title, imageUrl, body, createdAt }: ArticleData) => {
   return (
     <div className="flex flex-col items-center py-8">
       <article className="bg-white rounded-lg shadow-lg max-w-3xl w-full p-8">
@@ -19,7 +11,7 @@ const Article = ({ title, imageUrl, content, createdAt }: ArticleProps) => {
           <p className="text-sm text-gray-600">{createdAt}</p>
           <img src={imageUrl} alt={title} className="w-full h-auto my-4 rounded" />
           <Text className="mb-[16px] max-w-[700px]" variant="sm">
-            {content}
+            {body}
           </Text>
       </article>
     </div>
